deeda660484a5e4c50e989f2e0ebf1cfefee1b74
hongky <hongkypcx.hp@gmail.com>
2024-12-13 16:26:27 +0900

    v1

---
+++ Assets/UnityTechnologies/HMITemplate/Scripts/HMI/UI/Cluster/CenterController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace HMI.UI.Cluster
+{
+    /// <summary>
+    /// Controller for the center part of the cluster
+    /// </summary>
+    public class CenterController : MonoBehaviour
+    {
+        /// <summary>
+        /// Timeline to move the center of the cluster from Adas to Map Mode
+        /// </summary>
+        public PlayableAsset AdasToMap;
+        /// <summary>
+        /// Timeline to move the center of the cluster from Map to Adas Mode
+        /// </summary>
+        public PlayableAsset MapToAdas;
+
+        public PlayableAsset AdasToBEV;
+
+        /// <summary>
+        /// Director that plays timelines
+        /// </summary>
+        public PlayableDirector Director;
+
+        /// <summary>
+        /// Is the center of the cluster currently showing the Adas?
+        /// </summary>
+        private int Adas = 0;
+
+        /// <summary>
+        /// Next screen
+        /// </summary>
+        public void Next()
+        {
+            Switch();
+        }
+
+        /// <summary>
+        /// Previous screen
+        /// </summary>
+        public void Previous()
+        {
+            Switch(-1);
+        }
+
+        public void SwitchTo(int adas){
+            this.Adas = adas;
+            Switch();
+        }
+
+        /// <summary>
+        /// Switch Screens
+        /// </summary>
+        private void Switch(int alpha = 1)
+        {
+            if (this.Adas < 0) this.Adas = 2;
+            if (this.Adas > 2) this.Adas = 0;
+            switch (Adas)
+            {
+                case 0:
+                    Adas += alpha;
+                    Director.Play(AdasToBEV);
+                    break;
+                case 1:
+                    Adas += alpha;
+                    Director.Play(AdasToMap);
+                    break;
+                case 2:
+                    Adas += alpha;
+                    Director.Play(MapToAdas);
+                    break;
+            }
+
+        }
+       
+    }
+}
---
+++ Assets/UnityTechnologies/HMITemplate/Scripts/HMI/Vehicles/Behaviours/VehicleController.cs
@@ -0,0 +1,148 @@
+﻿using HMI.Vehicles.Services;
+using UnityEngine;
+using System.Collections.Generic;
+using HMI.Vehicles.Data;
+
+namespace HMI.Vehicles.Behaviours
+{
+    /// <summary>
+    /// Controls the vehicle
+    /// </summary>
+    public class VehicleController : MonoBehaviour
+    {
+        public IList<string> GearNames{
+
+            get { return VehicleService.GetTransmission().GearNames; }
+
+        }
+
+        public List<string> GeerableNames{
+
+            get { 
+                //  filter out the current gear
+                List<string> GearNames = new List<string>(VehicleService.GetTransmission().GearNames);
+                GearNames.Remove(CurrentGear.Gear);
+                return GearNames;
+                }
+
+        }
+
+        public GearData CurrentGear{
+
+            get { return VehicleService.GetTransmission().CurrentGear; }
+
+        }
+    
+ 
+        /// <summary>
+        /// Switch vehicle to next gear
+        /// </summary>
+        public void NextGear()
+        {
+            VehicleService.GetTransmission().SwitchToNextGear();
+        }
+
+        /// <summary>
+        /// Switch vehicle to previous gear
+        /// </summary>
+        public void PreviousGear()
+        {
+            VehicleService.GetTransmission().SwitchToPreviousGear();
+        }
+
+        /// <summary>
+        ///  SwitchToDesiredGear
+        ///  </summary>
+        public void SetGear(string gear)
+        {
+            //  check if the gear is valid
+            if (!GearNames.Contains(gear))
+            {
+                Debug.LogError("Invalid gear: " + gear);
+                return;
+            }
+            VehicleService.GetTransmission().SwitchToDesiredGear(gear);
+        }
+
+        /// <summary>
+        /// Accelerate vehicle
+        /// </summary>
+        public void Accelerate()
+        {
+            VehicleService.GetVehicle().Accelerate(1f);
+        }
+
+        /// <summary>
+        /// Brake vehicle
+        /// </summary>
+        public void Brake()
+        {
+            VehicleService.GetVehicle().Brake(1f);
+        }
+
+        /// <summary>
+        /// Start/Stop Engine
+        /// </summary>
+        public static void StartStopEngine()
+        {
+           var engine = VehicleService.GetEngine();
+
+            if(engine.IsEngineOn)
+            {
+                engine.TurnEngineOff();
+            }
+            else
+            {
+                engine.TurnEngineOn();
+            }
+        }
+
+        /// <summary>
+        /// Increase ADAS goal speed
+        /// </summary>
+        public static void AdasIncreaseGoalSpeed()
+        {
+            VehicleService.GetSpeedController().IncreaseGoalSpeed();
+        }
+
+        /// <summary>
+        /// Decrease ADAS goal speed
+        /// </summary>
+        public static void AdasDecreaseGoalSpeed()
+        {
+            VehicleService.GetSpeedController().DecreaseGoalSpeed();
+        }
+
+        /// <summary>
+        /// Set ADAS 
+        /// </summary>
+        public static void AdasSet()
+        {
+            VehicleService.GetSpeedController().SetAutomaticSpeedControl();
+        }
+
+        /// <summary>
+        /// Cancel ADAS command
+        /// </summary>
+        public static void AdasCancel()
+        {
+            VehicleService.GetSpeedController().CancelAutomaticSpeedControl();
+        }
+
+
+        // <summary>
+        /// Set togle for ADAS
+        /// </summary>
+        public static void AdasToggle()
+        {
+            if (VehicleService.GetSpeedController().Data.IsSpeedControlActive)
+            {
+                VehicleService.GetSpeedController().CancelAutomaticSpeedControl();
+            }
+            else
+            {
+                VehicleService.GetSpeedController().SetAutomaticSpeedControl();
+            }
+        }
+    }
+}
